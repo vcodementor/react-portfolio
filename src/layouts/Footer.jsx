6fb51bb5696d2a21b52d6b0cec5c611ff91d4e58
vcodementor <vcodementor@gmail.com>
2023-02-25 20:54:01 -0500

    Initial commit message

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -3,7 +3,7 @@ export default function footer() {
     <footer className="mx-1 mt-8 md:mt-0">
       <p className="text-center text-[0.875rem] text-almost-black dark:text-white">
         Coded by{" "}
-        <a href="#"
+        <a href="#about"
           target="_blank"
           rel="noreferrer noopener"
           className="font-bold hover:text-medium-gray"
